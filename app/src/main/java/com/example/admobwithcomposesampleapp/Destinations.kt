02ae3bbefbe349fde2c8a530a064c5231a075038
kkkkan <kan4649kan@gmail.com>
2024-02-12 16:56:48 +0900

    feat : タブを追加。

---
+++ app/src/main/java/com/example/admobwithcomposesampleapp/Destinations.kt
@@ -0,0 +1,18 @@
+package com.example.admobwithcomposesampleapp
+
+interface Destinations {
+    val route: String
+}
+
+object Top : Destinations {
+    override val route = "top"
+
+    enum class Tabs(
+        val tabName: String,
+        val tabIndex: Int
+    ) {
+        Banner("バナー", 0),
+        Native("ネイティブ", 1),
+        Interstitial("インタースティシャル", 2)
+    }
+}
--- app/src/main/java/com/example/admobwithcomposesampleapp/MainActivity.kt
+++ app/src/main/java/com/example/admobwithcomposesampleapp/MainActivity.kt
@@ -6,15 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.admobwithcomposesampleapp.screen.TopScreen
 import com.example.admobwithcomposesampleapp.ui.theme.AdMobWithComposeSampleAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             AdMobWithComposeSampleAppTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +26,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    NavigationHost()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    AdMobWithComposeSampleAppTheme {
-        Greeting("Android")
+fun NavigationHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+    startDestination: String = Top.route
+) {
+    NavHost(
+        modifier = modifier,
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(route = Top.route) {
+            TopScreen()
+        }
     }
 }
