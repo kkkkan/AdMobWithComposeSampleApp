46627b4c4ba7412490ab856f82d1187933ecb092
kkkkan <kan4649kan@gmail.com>
2024-02-12 18:08:27 +0900

    feat : バナー広告を追加。表示できるようになった。

--- app/src/main/java/com/example/admobwithcomposesampleapp/screen/TopScreen.kt
+++ app/src/main/java/com/example/admobwithcomposesampleapp/screen/TopScreen.kt
@@ -11,11 +11,17 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.example.admobwithcomposesampleapp.Top
+import com.example.admobwithcomposesampleapp.screen.contents.BannerTabContent
+import com.google.android.gms.ads.AdView
 
 @Composable
-fun TopScreen() {
+fun TopScreen(
+    // 使用するバナー広告
+    // リストのスクロール量が変更して表示非表示が切り替わった場合に毎回loadAdを呼んだりしないように
+    // Activityが作成された時に作って、以降はそれを使いまわす
+    banner : AdView
+) {
     Column(modifier = Modifier.fillMaxSize()) {
-
         val selectedTab = remember {
             // 最初に表示するのはバナー広告のタブ
             mutableStateOf(Top.Tabs.Banner.tabIndex)
@@ -34,7 +40,7 @@ fun TopScreen() {
 
         when (selectedTab.value) {
             Top.Tabs.Banner.tabIndex -> {
-                // TODO
+                BannerTabContent(banner =banner )
             }
 
             Top.Tabs.Native.tabIndex -> {
