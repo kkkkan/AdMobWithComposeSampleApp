7b89104bef0484e9d3493f6e192b19ba0079948c
kkkkan <kan4649kan@gmail.com>
2024-02-24 15:30:14 +0900

    feat : リワード広告を表示できるようにしました。

---
+++ app/src/main/java/com/example/admobwithcomposesampleapp/screen/contents/RewardAdTab.kt
@@ -0,0 +1,77 @@
+package com.example.admobwithcomposesampleapp.screen.contents
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.admobwithcomposesampleapp.viewmodel.RewardAdViewModel
+
+@Composable
+fun RewardAdTabContent(
+    viewModel: RewardAdViewModel = hiltViewModel()
+) {
+    val context = LocalContext.current
+    val state = viewModel.uiState.collectAsState()
+    Column {
+        Spacer(modifier = Modifier.weight(1f))
+
+        Text(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+                .background(
+                    color = Color.Red
+                )
+                .clickable {
+                    viewModel.loadRewardAdIfNeed(context)
+                }
+                .padding(16.dp),
+            style = androidx.compose.ui.text.TextStyle(
+                color = Color.White
+            ),
+            textAlign = TextAlign.Center,
+            text = "リワード広告読み込み開始"
+        )
+        Spacer(modifier = Modifier.weight(1f))
+
+        // 広告を表示ボタンの背景色は広告を読み込み済みかどうかで変える
+        val showButtonBackgroundColor = if (state.value.canShowAd) {
+            Color.Red
+        } else {
+            Color.Gray
+        }
+
+        Text(
+            modifier = Modifier
+                .padding(horizontal = 16.dp)
+                .fillMaxWidth()
+                .background(
+                    color = showButtonBackgroundColor
+                )
+                .clickable {
+                    if (state.value.canShowAd) {
+                        // 広告を読み込み済みだったら表示する
+                        viewModel.showRewardAd()
+                    }
+                }
+                .padding(16.dp),
+            style = androidx.compose.ui.text.TextStyle(
+                color = Color.White
+            ),
+            textAlign = TextAlign.Center,
+            text = "リワード広告表示"
+        )
+        Spacer(modifier = Modifier.weight(1f))
+    }
+}
--- app/src/main/java/com/example/admobwithcomposesampleapp/viewmodel/RewardAdViewModel.kt
+++ app/src/main/java/com/example/admobwithcomposesampleapp/viewmodel/RewardAdViewModel.kt
@@ -8,8 +8,9 @@ import com.google.android.gms.ads.AdError
 import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.FullScreenContentCallback
 import com.google.android.gms.ads.LoadAdError
-import com.google.android.gms.ads.interstitial.InterstitialAd
-import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+import com.google.android.gms.ads.rewarded.RewardItem
+import com.google.android.gms.ads.rewarded.RewardedAd
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -17,16 +18,16 @@ import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 /**
- * インタースティシャル広告関係のViewModel
+ * リワード広告関係のViewModel
  */
 @HiltViewModel
-class InterstitialAdViewModel @Inject constructor(
+class RewardAdViewModel @Inject constructor(
 ) : ViewModel() {
     data class UiState(
         // 広告が表示できる状態かどうか
         val canShowAd: Boolean = false,
-        // インステ広告を表示する
-        val showInterstitialAd: Boolean = false
+        // リワード広告を表示する
+        val showRewardAd: Boolean = false
     )
 
     private val _uiState = MutableStateFlow(UiState())
@@ -34,9 +35,9 @@ class InterstitialAdViewModel @Inject constructor(
         get() = _uiState.asStateFlow()
 
 
-    // インタースティシャル広告
+    // リワード広告
     // プロセス再生成には対応していません。
-    private var interstitialAd: InterstitialAd? = null
+    private var rewardAd: RewardedAd? = null
         set(value) {
             field = value
             _uiState.value = _uiState.value.copy(canShowAd = (value != null))
@@ -44,22 +45,22 @@ class InterstitialAdViewModel @Inject constructor(
 
     private var fullScreenContentCallback: FullScreenContentCallback? = null
 
-    private var isInterstitialAdNowLoading = false
+    private var isRewardAdNowLoading = false
 
-    // インステ広告表示のフラグを立てる
-    fun showInterstitialAd() {
-        _uiState.value = _uiState.value.copy(showInterstitialAd = true)
+    // リワード広告表示のフラグを立てる
+    fun showRewardAd() {
+        _uiState.value = _uiState.value.copy(showRewardAd = true)
     }
 
-    fun consumedInterstitialAd() {
-        _uiState.value = _uiState.value.copy(showInterstitialAd = false)
+    fun consumedRewardAd() {
+        _uiState.value = _uiState.value.copy(showRewardAd = false)
     }
 
     /**
-     * インタースティシャル広告を読み込み開始
+     * リワード広告を読み込み開始
      */
-    fun loadInterstitialAdIfNeed(ctx: Context) {
-        if (isInterstitialAdNowLoading || interstitialAd != null) {
+    fun loadRewardAdIfNeed(ctx: Context) {
+        if (isRewardAdNowLoading || rewardAd != null) {
             // 既に読み込み済みor読み込み中だったら、何もしない
             return
         }
@@ -67,49 +68,57 @@ class InterstitialAdViewModel @Inject constructor(
         fullScreenContentCallback = object : FullScreenContentCallback() {
             override fun onAdFailedToShowFullScreenContent(var1: AdError) {
                 // 表示に失敗しても、おそらく広告の再利用はできないので捨てる。
-                interstitialAd = null
+                rewardAd = null
             }
 
             override fun onAdShowedFullScreenContent() {
                 // 表示を開始
                 // 広告の再利用はできないので捨てる。
-                interstitialAd = null
+                rewardAd = null
             }
         }
 
         // 読み込み開始のフラグを立てる
-        isInterstitialAdNowLoading = true
+        isRewardAdNowLoading = true
         val adRequest = AdRequest.Builder().build()
 
-        InterstitialAd.load(
+        RewardedAd.load(
             ctx,
-            ctx.getString(R.string.admob_interstitial_unit_id),
+            ctx.getString(R.string.admob_reward_unit_id),
             adRequest,
-            object : InterstitialAdLoadCallback() {
+            object : RewardedAdLoadCallback() {
                 override fun onAdFailedToLoad(adError: LoadAdError) {
                     // フラグを下す
-                    isInterstitialAdNowLoading = false
-                    interstitialAd = null
+                    isRewardAdNowLoading = false
+                    rewardAd = null
                 }
 
-                override fun onAdLoaded(ad: InterstitialAd) {
+                override fun onAdLoaded(ad: RewardedAd) {
                     // フラグを下す
-                    isInterstitialAdNowLoading = false
+                    isRewardAdNowLoading = false
 
-                    interstitialAd = ad
-                    interstitialAd!!.fullScreenContentCallback = fullScreenContentCallback
+                    rewardAd = ad
+                    rewardAd!!.fullScreenContentCallback = fullScreenContentCallback
                 }
             })
     }
 
     /**
-     * 可能なタイミングと状況なら、インタースティシャル広告を見せる。
+     * 可能なタイミングと状況なら、リワード広告を見せる。
      */
-    fun openInterstitialAdIfCan(activity: Activity) {
-        if (interstitialAd != null) {
+    fun openRewardAdIfCan(
+        activity: Activity,
+        onEarnedReward: (rewardItem: RewardItem) -> Unit
+    ) {
+        if (rewardAd != null) {
             // 共有直後かつインタースティシャル広告の読み込みが終わっていれば表示。
-            // フラグを下したりは、interstitialAdの表示コールバックの方で行っている。
-            interstitialAd!!.show(activity)
+            // フラグを下したりは、RewardAdの表示コールバックの方で行っている。
+            rewardAd!!.show(activity) { rewardItem ->
+                // TODO
+                // リワード広告を見た後の処理
+                // ここでリワードを付与する処理を行う
+                onEarnedReward(rewardItem)
+            }
         }
     }
 
